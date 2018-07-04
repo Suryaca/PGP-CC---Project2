4b65edd82892e4c72a1b65db02d9e9754ef35cd3
Surya C Akasam <suryac.a@checkon.co>
2018-07-05 00:24:34 +0530

    Edited with Subsxription code

--- app.py
+++ app.py
@@ -20,7 +20,21 @@ def processInvoice():
     return " JSON Message: " + json.dumps(request.json)
 
 @app.route("/sns-subscribe", methods=['GET','POST','PUT'])
-def sns():
+def handle_requests():
+    global messages
+
+    if request.method == 'POST':
+        sys.stdout.write("Message Recieved")
+        request_content = json.loads(request.get_data())
+        sys.stdout.write(str(request_content))
+
+'''        # Insert newest messages to front of list
+        messages.insert(0, request_content["Message"])
+        return str(len(messages))
+
+    if request.method == 'GET':
+        return render_template('SNS.html', message_queue=messages)'''
+'''def sns():
     try:
         js=json.loads(request.data)
     except:
@@ -30,13 +44,13 @@ def sns():
     #Sucbscribe to SNS Topic
     if hdr== 'SubscriptionConformation' and 'SubscribeURL' in js:
         r =requests.get(js['SubscribeURL'])
-        print(js,sys.stderr)
+        print(r,sys.stderr)
         #return js
 
     #if hdr == 'Notification':
     #    msg_process(js['Message'],js['Timestamp'])
 
-    return 'OK\n'
+    return 'OK\n' '''
 
 if __name__ == '__main__':
     app.run(
