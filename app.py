cfa1358244351d7ab98ce70a9da5ab77046baec4
Surya C Akasam <suryac.a@checkon.co>
2018-07-05 00:40:34 +0530

    edited

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ def handle_requests():
     hdr = request.headers.get('X-Amz-Sns-Message-Type')
     if hdr== 'SubscriptionConformation' and 'SubscribeURL' in js:
         r =requests.get(js['SubscribeURL'])
-        sys.stdout.write(r)
+        sys.stdout.write("\n\n SubscribeURL %s" str(r))
+        #sys.stdout.write(r)
 
     return "OK"
 
