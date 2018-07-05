dc90293a6967ea9972334a70e232e00d5b6d2e5e
Surya C Akasam <suryac.a@checkon.co>
2018-07-05 19:34:48 +0530

    new

--- app.py
+++ app.py
@@ -20,15 +20,15 @@ def handle_requests():
     sys.stdout.write(str(request_content))
 '''
     if request.method == 'POST':
-        #sys.stdout.write("\n\n\Message Recieved\n\n")
+        sys.stdout.write("\n\n\Message Recieved\n\n")
         request_content = json.loads(request.get_data())
-        #sys.stdout.write(str(request_content))
-        json.loads(request_content['Message'])
+        sys.stdout.write(str(request_content))
+        #json.loads(request_content['Message'])
         sys.stdout.write("\n\n Notification \n\n")
         #sys.stdout.write(str(request_content[s3][object]))
         #x = json.loads(request_content('Message'))
-        sys.stdout.write("\n\n Notification Recieved\n\n")
-        sys.stdout.write(str(x))
+        #sys.stdout.write("\n\n Notification Recieved\n\n")
+        #sys.stdout.write(str(x))
 
     return "OK"
 
