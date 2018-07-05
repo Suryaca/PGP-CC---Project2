0f6d276acd64622420c8c1611a93be506673b958
Surya C Akasam <suryac.a@checkon.co>
2018-07-05 16:27:48 +0530

    update

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def handle_requests():
         sys.stdout.write("\n\n\Message Recieved\n\n")
         request_content = json.loads(request.get_data())
         sys.stdout.write(str(request_content))
-        json.loads(request_content['Message'])
+        #json.loads(request_content['Message'])
         x = json.loads(request_content['Message'])
         sys.stdout.write("\n\n Notification Recieved\n\n")
         sys.stdout.write(str(x))
